d370481ad3060819991fa42b9ea854435000fba4
GomenaSorry <ruzzellfernandez28@gmail.com>
2022-11-24 00:04:08 +0800

    added new tests

--- cypress/integration/CommunityPageTests.js
+++ cypress/integration/CommunityPageTests.js
@@ -7,6 +7,9 @@ describe('Community Page tests', () => {
     const dateToday = moment().format('DD MMM YYYY')
     const firstName = 'John'
     const lastName = 'Doe'
+    const imageWidth = 286
+    const imageLength = 342
+    const cardStatus = ['DRAFT', 'PENDING FOR APPROVAL', 'REJECTED', 'FOR PUBLISH', 'PUBLISHED', 'EXPIRED', 'UNPUBLISHED' ]
 
     beforeEach(() => {
         cy.viewport(1920, 1080)
@@ -15,9 +18,10 @@ describe('Community Page tests', () => {
 
     it('check if the user is the Community Management module', () => {
         cy.get('.nav-link').should('have.text', 'Community Management')
+        cy.location('href').should('eq', 'https://dpbdboo.github.io/Testing-App/#')
     })
 
-    it('check if all posts are visible', () => {
+    it('check if all posts are visible and equal to 4', () => {
         cy.get('div.row').find('div.card').should('have.length', 4)
     })
 
@@ -47,67 +51,95 @@ describe('Community Page tests', () => {
         })
     })
 
+    it('check if each card status is valid', () => {
+        cy.get('.row').find('.card').each(($el, index, $list) => {
+            cy.log('card ' + (index + 1)).then(() => {
+                cy.wrap($el).find('.m-auto').invoke('text').then((statusText) => {
+                    cy.log(statusText)
+                    expect(cardStatus.includes(statusText)).to.be.true
+                })
+            })
+        })
+    })
+
     it('check if a removed post is visible', () => {
         cy.get('.m-auto').should('not.have.text', 'REMOVED')
     })
 
     it('check if a post will go live today', () => {
-        cy.get(':nth-child(2) > .card > .card-body > :nth-child(10)').invoke('text').then((expiryDate) => {
+        cy.get(communityPage.testCardGoLiveTodayExpiryDate).invoke('text').then((expiryDate) => {
             const testDate = moment(expiryDate).format('DD MMM YYYY')
-            expect(moment(testDate).isAfter(moment(cy.get(':nth-child(2) > .card > .card-body > :nth-child(8)').invoke('text')))).to.be.true  
+            expect(moment(testDate).isAfter(moment(cy.get(communityPage.testCardGoLiveTodayDateCreated).invoke('text')))).to.be.true
+            expect(moment(testDate).isAfter(moment(cy.get(communityPage.testCardGoLiveTodayPostingDate).invoke('text')))).to.be.true
+            expect(moment(cy.get(communityPage.testCardGoLiveTodayDateCreated).invoke('text')).isSameOrBefore(moment(cy.get(communityPage.testCardGoLiveTodayPostingDate)))).to.be.true 
         })
-        cy.get(':nth-child(2) > .card > .card-body > :nth-child(8)').invoke('text').then((postDate) => {
+
+        cy.get(communityPage.testCardGoLiveTodayPostingDate).invoke('text').then((postDate) => {
             expect(postDate).to.be.eq(dateToday)
         })
-        cy.get(':nth-child(2) > .card > .img-container > .test > .notification-container').should('contain', 'Will go live today')
-        cy.get(':nth-child(2) > .card > .card-body > .text-center > .m-auto').should('have.text', 'FOR PUBLISH')
+
+        cy.get(communityPage.testCardGoLiveTodayNotificationContainer).should('contain', 'Will go live today')
+
+        cy.get(communityPage.testCardGoLiveTodayStatus).should('have.text', 'FOR PUBLISH')
     })
 
     it('check if a post will go live in x days', () => {
-        cy.get(':nth-child(3) > .card > .card-body > :nth-child(10)').invoke('text').then((expiryDate) => {
+        cy.get(communityPage.testCardGoLiveFutureExpiryDate).invoke('text').then((expiryDate) => {
             const testDate = moment(expiryDate).format('DD MMM YYYY')
-            expect(moment(testDate).isAfter(moment(cy.get(':nth-child(3) > .card > .card-body > :nth-child(8)').invoke('text')))).to.be.true  
+            expect(moment(testDate).isAfter(moment(cy.get(communityPage.testCardGoLiveFuturePostingDate).invoke('text')))).to.be.true
+            expect(moment(testDate).isAfter(moment(cy.get(communityPage.testCardGoLiveFutureDateCreated).invoke('text')))).to.be.true
+            expect(moment(cy.get(communityPage.testCardGoLiveFuturePostingDate).invoke('text')).isSameOrBefore(moment(cy.get(communityPage.testCardGoLiveFutureDateCreated)))).to.be.true  
         })
 
-        cy.get(':nth-child(3) > .card > .img-container > .test > .notification-container').invoke('text').then((getText) => {
+        cy.get(communityPage.testCardGoLiveFutureNotificationContainer).invoke('text').then((getText) => {
             const numberOfDays = getText.match(/\d+/)[0]
-            cy.get(':nth-child(3) > .card > .card-body > :nth-child(8)').invoke('text').then((postDate) => {
+            cy.get(communityPage.testCardGoLiveFuturePostingDate).invoke('text').then((postDate) => {
                 var testDate = moment(postDate).format('DD MMM YYYY')
                 var result = moment(testDate).diff(dateToday, 'days')
                 expect(result.toString()).to.be.eq(numberOfDays)
+            }).then(() => {
+                cy.get(communityPage.testCardGoLiveFutureNotificationContainer).should('contain', 'Will go live in ' + numberOfDays + ' days')
             })
         })
 
-        cy.get(':nth-child(3) > .card > .card-body > .text-center > .m-auto').should('have.text', 'FOR PUBLISH')
+        cy.get(communityPage.testCardGoLiveFutureStatus).should('have.text', 'FOR PUBLISH')
     })
 
     it('check if a post will go expire in x days', () => {
-        cy.get(':nth-child(1) > .card > .card-body > :nth-child(10)').invoke('text').then((expiryDate) => {
+        cy.get(communityPage.testCardWillExpireExpiryDate).invoke('text').then((expiryDate) => {
             const testDate = moment(expiryDate).format('DD MMM YYYY')
-            expect(moment(testDate).isAfter(moment(cy.get(':nth-child(2) > .card > .card-body > :nth-child(8)').invoke('text')))).to.be.true  
+            expect(moment(testDate).isAfter(moment(cy.get(communityPage.testCardWillExpirePostingDate).invoke('text')))).to.be.true  
+            expect(moment(testDate).isAfter(moment(cy.get(communityPage.testCardWillExpireDateCreated).invoke('text')))).to.be.true
+            expect(moment(cy.get(communityPage.testCardWillExpireDateCreated).invoke('text')).isSameOrBefore(moment(cy.get(communityPage.testCardWillExpirePostingDate)))).to.be.true  
         })
 
-        cy.get(':nth-child(1) > .card > .img-container > .test > .notification-container').invoke('text').then((getText) => {
+        cy.get(communityPage.testCardWillExpireNotificationContainer).invoke('text').then((getText) => {
             const numberOfDays = getText.match(/\d+/)[0]
-            cy.get(':nth-child(1) > .card > .card-body > :nth-child(10)').invoke('text').then((expiryDate) => {
+            cy.get(communityPage.testCardWillExpireExpiryDate).invoke('text').then((expiryDate) => {
                 var testDate = moment(expiryDate).format('DD MMM YYYY')
                 var result = moment(testDate).diff(dateToday, 'days')
                 // code test app is showing wrong day value, should be 7 instead of 5
                 //expect((result).toString()).to.be.eq(numberOfDays)
                 expect((result-2).toString()).to.be.eq(numberOfDays)
+            }).then(() => {
+                cy.get(communityPage.testCardWillExpireNotificationContainer).should('contain', 'Will expire in ' + numberOfDays + ' days')
             })
         })
 
-        cy.get(':nth-child(3) > .card > .card-body > .text-center > .m-auto').should('have.text', 'FOR PUBLISH')
+        cy.get(communityPage.testCardWillExpireStatus).should('have.text', 'PUBLISHED')
     })
 
     it('check if a post is expired', () => {
-        cy.get(':nth-child(4) > .card > .img-container > .test > .notification-container').should('not.exist');
-        cy.get(':nth-child(4) > .card > .card-body > :nth-child(10)').invoke('text').then((expiryDate) => {
+        cy.get(communityPage.testCardExpiredExpiryDate).invoke('text').then((expiryDate) => {
             const testDate = moment(expiryDate).format('DD MMM YYYY')
-            expect(moment(dateToday).isAfter(moment(testDate))).to.be.true  
+            expect(moment(dateToday).isAfter(moment(testDate))).to.be.true
+            expect(moment(testDate).isBefore(moment(dateToday))).to.be.true
+            expect(moment(cy.get(communityPage.testCardExpiredDateCreated).invoke('text')).isSameOrBefore(moment(cy.get(communityPage.testCardExpiredPostingDate)))).to.be.true   
         })
-        cy.get(':nth-child(4) > .card > .card-body > .text-center > .m-auto').should('have.text', 'EXPIRED')
+
+        cy.get(communityPage.testCardExpiredNotificationContainer).should('not.exist');
+
+        cy.get(communityPage.testCardExpiredStatus).should('have.text', 'EXPIRED')
     })
 
     it('check if all post title are less than 45 characters', () => {
@@ -119,7 +151,7 @@ describe('Community Page tests', () => {
         })
     })
 
-    it('check if all dates are in the correct format', () => {
+    it('check if the dates on each card are in the correct format', () => {
         cy.get('.row').find('.card').each(($el, index, $list) => {
             cy.log('card ' + (index + 1)).then(() => {
                 const DATE_FORMAT = moment().format('DD MMM YYYY')
@@ -133,7 +165,7 @@ describe('Community Page tests', () => {
         })
     })
 
-    it('check if all cards have the correct author name format', () => {
+    it('check if each card displays the correct author name format', () => {
         cy.get('.row').find('.card').each(($el, index, $list) => {
             cy.log('card ' + (index + 1)).then(() => {
                 cy.get(':nth-child(1) > .card > .card-body > :nth-child(4)').should('have.text', firstName + ' ' + lastName)
@@ -141,7 +173,7 @@ describe('Community Page tests', () => {
         })
     })
 
-    it('check if all cards have status', () => {
+    it('check if each cards has status text', () => {
         cy.get('.row').find('.card').each(($el, index, $list) => {
             cy.log('card ' + (index + 1)).then(() => {
                 cy.wrap($el).find('.m-auto').should('exist')
@@ -149,17 +181,17 @@ describe('Community Page tests', () => {
         })
     })
 
-    it('check if card image container have the correct size', () => {
+    it('check if each card image container has the correct size', () => {
         cy.get('.row').find('.img-container').each(($el, index, $list) => {
             cy.log('card ' + (index + 1)).then(() => {
-                cy.wrap($el).invoke('css', 'width').then(str => parseInt(str)).should('eq', 286).then(() =>{
-                    cy.wrap($el).invoke('css', 'length').then(str => parseInt(str)).should('eq', 342)
+                cy.wrap($el).invoke('css', 'width').then(str => parseInt(str)).should('eq', imageWidth).then(() =>{
+                    cy.wrap($el).invoke('css', 'length').then(str => parseInt(str)).should('eq', imageLength)
                 })
             })
         })
     })
 
-    it('check if cards are fixed position', () => {
+    it('check if each card is in fixed position', () => {
         cy.get('.row').find('.col-xs-12.col-sm-3').each(($el, index, $list) => {
             cy.log('card ' + (index + 1)).then(() => {
                 cy.wrap($el).invoke('css', 'display').should('eq', 'block')
--- cypress/support/page-objects/CommunityPage.js
+++ cypress/support/page-objects/CommunityPage.js
@@ -3,5 +3,29 @@ export class CommunityPage{
     navigateToCommunityPage(){
         cy.visit(Cypress.env('url'))
     }
+
+    testCardWillExpireDateCreated = ':nth-child(1) > .card > .card-body > :nth-child(6)'
+    testCardWillExpirePostingDate = ':nth-child(1) > .card > .card-body > :nth-child(8)'
+    testCardWillExpireExpiryDate = ':nth-child(1) > .card > .card-body > :nth-child(10)'
+    testCardWillExpireStatus = ':nth-child(1) > .card > .card-body > .text-center > .m-auto'
+    testCardWillExpireNotificationContainer = ':nth-child(1) > .card > .img-container > .test > .notification-container'
+
+    testCardGoLiveTodayStatus = ':nth-child(2) > .card > .card-body > .text-center > .m-auto'
+    testCardGoLiveTodayDateCreated = ':nth-child(2) > .card > .card-body > :nth-child(6)'
+    testCardGoLiveTodayPostingDate = ':nth-child(2) > .card > .card-body > :nth-child(8)'
+    testCardGoLiveTodayExpiryDate = ':nth-child(2) > .card > .card-body > :nth-child(10)'
+    testCardGoLiveTodayNotificationContainer = ':nth-child(2) > .card > .img-container > .test > .notification-container'
+
+    testCardGoLiveFutureDateCreated = ':nth-child(3) > .card > .card-body > :nth-child(6)'
+    testCardGoLiveFuturePostingDate = ':nth-child(3) > .card > .card-body > :nth-child(8)'
+    testCardGoLiveFutureExpiryDate = ':nth-child(3) > .card > .card-body > :nth-child(10)'
+    testCardGoLiveFutureStatus = ':nth-child(3) > .card > .card-body > .text-center > .m-auto'
+    testCardGoLiveFutureNotificationContainer = ':nth-child(3) > .card > .img-container > .test > .notification-container'
+
+    testCardExpiredDateCreated = ':nth-child(4) > .card > .card-body > :nth-child(6)'
+    testCardExpiredPostingDate = ':nth-child(4) > .card > .card-body > :nth-child(8)'
+    testCardExpiredExpiryDate = ':nth-child(4) > .card > .card-body > :nth-child(10)'
+    testCardExpiredStatus = ':nth-child(4) > .card > .card-body > .text-center > .m-auto'
+    testCardExpiredNotificationContainer = ':nth-child(4) > .card > .img-container > .test > .notification-container'
     
 }
